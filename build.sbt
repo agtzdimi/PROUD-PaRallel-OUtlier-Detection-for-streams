3d093f9053a61f75ac959766f06e271aa5353495
agtzdimi <agtzdimi@gmail.com>
2020-08-11 16:26:46 +0300

    Initial commit with changes in custom_source files to use spark streaming context
    The initial commit was to create a spark streaming context with usage of Kafka to listen to the environment variables set for Kafka Brokers and Topics and receive input from custom source

--- build.sbt
+++ build.sbt
@@ -13,12 +13,28 @@ val flinkDependencies = Seq(
   "org.apache.bahir" %% "flink-connector-influxdb" % "1.1-SNAPSHOT"
 )
 
+val sparkVersion = "2.4.5"
+
 lazy val root = (project in file(".")).
   settings(
     name := "PROUD",
     libraryDependencies ++= flinkDependencies,
     libraryDependencies += "org.apache.kafka" % "kafka-clients" % "2.2.1"
   )
+
+libraryDependencies ++= Seq(
+  "org.apache.spark" %% "spark-core" % sparkVersion,
+  "org.apache.spark" %% "spark-sql" % sparkVersion,
+  "org.apache.spark" %% "spark-mllib" % sparkVersion,
+  "org.apache.spark" %% "spark-streaming" % sparkVersion,
+  "org.apache.spark" %% "spark-hive" % sparkVersion,
+  "org.apache.spark" %% "spark-mllib-local" % sparkVersion
+)
+
+libraryDependencies += "com.github.scopt" % "scopt_2.11" % "4.0.0-RC2"
+libraryDependencies += "org.apache.spark" % "spark-streaming_2.11" % sparkVersion
+libraryDependencies += "org.apache.spark" % "spark-streaming-kafka-0-10-assembly_2.11" % sparkVersion
+
 
 assembly / mainClass := Some("outlier_detection.Outlier_detection")
 
--- src/main/scala/custom_source/Custom_source.scala
+++ src/main/scala/custom_source/Custom_source.scala
@@ -1,39 +1,93 @@
 package custom_source
 
-import java.util.Properties
-
-import org.apache.flink.api.common.serialization.SimpleStringSchema
-import org.apache.flink.api.java.utils.ParameterTool
-import org.apache.flink.streaming.api.scala.StreamExecutionEnvironment
-import org.apache.flink.api.scala.createTypeInformation
-import org.apache.flink.streaming.connectors.kafka.FlinkKafkaProducer
+import scopt.OParser
+import org.apache.spark.streaming.{Seconds, StreamingContext}
+import org.apache.kafka.common.serialization.StringDeserializer
+import org.apache.spark.streaming.kafka010._
+import org.apache.spark.streaming.kafka010.LocationStrategies.PreferConsistent
+import org.apache.spark.streaming.kafka010.ConsumerStrategies.Subscribe
 
 object Custom_source {
 
   def main(args: Array[String]): Unit = {
 
-    val parameters: ParameterTool = ParameterTool.fromArgs(args)
-    val dataset = parameters.getRequired("dataset") //STK, TAO
-    val DEBUG = parameters.get("DEBUG", "false").toBoolean
+    case class Config(
+                       DEBUG: Boolean = false,
+                       dataset: String = "",
+                       checkPointDir: String = "",
+                       kwargs: Map[String, String] = Map())
 
-    val output = System.getenv("KAFKA_TOPIC") //File input or kafka topic
-    val kafka_brokers = System.getenv("KAFKA_BROKERS")
-    // Properties for Kafka producers
-    val properties = new Properties()
-    properties.setProperty("bootstrap.servers", kafka_brokers)
-    //Create the kafka producer for the output topic (Law & Order)
-    val my_producer = new FlinkKafkaProducer[String](properties.getProperty("bootstrap.servers"), output, new SimpleStringSchema())
+    val builder = OParser.builder[Config]
 
-    //Start context
-    val env = StreamExecutionEnvironment.getExecutionEnvironment
-    env.setParallelism(1)
+    val parser = {
+      import builder._
+      OParser.sequence(
+        programName("PROUD Spark Streaming"),
+        head("scopt", "4.x"),
+        opt[Boolean]("DEBUG")
+          .action((x, c) => c.copy(DEBUG = x))
+          .text("DEBUG is a Boolean property"),
+        opt[String]("dataset")
+          .action((x, c) => c.copy(dataset = x))
+          .validate( x => {
+            if (x == "TAO" || x == "STK") success
+            else failure("dataset property can only be set as [TAO|STK]")
+          })
+          .text("Dataset property contains the type of dataset that will be used in the PROUD application" +
+            "It can either be set to 'TAO' or 'STK'"),
+        opt[String]("checkPointDir")
+          .action((x, c) => c.copy(dataset = x))
+          .validate( x => {
+            if (x.startsWith("/")) success
+            else failure("The Checkpoint Directory should be a full path directory starting with '/'")
+          })
+          .text("checkPointDir contains the full path directory to store the spark checkpoint of the application"),
 
-    val myGenerator = env
-      .addSource(new Kafka_flink_source(dataset))
-    if(DEBUG) myGenerator.print
-    else myGenerator.addSink(my_producer)
+      )
+    }
 
-    env.execute("Custom Flink non-parallel source")
+    var arguments: Config = Config()
+
+    // OParser.parse returns Option[Config]
+     OParser.parse(parser, args, Config()) match {
+      case Some(config) =>
+        arguments = Config(config.DEBUG,config.dataset,config.checkPointDir)
+      case _ =>
+      // arguments are bad, error message will have been displayed
+    }
+
+    val kafkaParams = Map[String, Object](
+      "bootstrap.servers" -> System.getenv("KAFKA_BROKERS"),
+      "key.deserializer" -> classOf[StringDeserializer],
+      "value.deserializer" -> classOf[StringDeserializer],
+      "group.id" -> "use_a_separate_group_id_for_each_stream",
+      "auto.offset.reset" -> "latest",
+      "enable.auto.commit" -> (false: java.lang.Boolean)
+    )
+
+    // Create the context with a 1 second batch size
+    val ssc = new StreamingContext("local[*]", "PROUD Spark Streaming", Seconds(1))
+
+    val topics = List(System.getenv("KAFKA_TOPIC"))
+    val stream = KafkaUtils.createDirectStream[String, String](
+      ssc,
+      PreferConsistent,
+      Subscribe[String, String](topics, kafkaParams)
+    )
+
+    val jsonProducer = new Json_source(arguments.dataset)
+
+
+    val data = stream.map(record => (record.key, record.value))
+
+    data.map(v => {
+      val record = jsonProducer.get_next()
+      println(record)
+    })
+    // Kick it off
+    ssc.checkpoint(arguments.checkPointDir)
+    ssc.start()
+    ssc.awaitTermination()
   }
 
 }
